11c085b927a8f2408a58c209acde1c465bdffdfc
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-04-25 20:59:56 -0300

    Treatment for empty question field

--- src/app/controllers/MessageController.js
+++ src/app/controllers/MessageController.js
@@ -1,8 +1,11 @@
 class MessageController {
   async askTutor (req, res) {
     const question = req.body
+    if (!question.content) {
+      return res.status(400).json({ error: "There's no question " })
+    }
     const answer = { content: 'Olá, seja bem vindo ao seu Tutor de algoritmos' }
-    return res.json({
+    return res.status(200).json({
       question,
       answer
     })

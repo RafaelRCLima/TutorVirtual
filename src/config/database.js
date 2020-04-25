34674254f407076bfee4c024a986add08999bd9f
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-04-25 16:09:05 -0300

    Test and Development databases are now separate

--- src/config/database.js
+++ src/config/database.js
@@ -20,7 +20,7 @@ module.exports = {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    database: process.env.DB_TEST,
     define: {
       timestamps: true,
       underscored: true,
